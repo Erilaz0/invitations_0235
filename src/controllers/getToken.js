ea1ef1f8913d61a6d6313d229f26b8372049f025
greenlaz <erilazerilaz@gmail.com>
2024-06-07 15:19:33 -0300

    final_changes2

--- src/controllers/getToken.js
+++ src/controllers/getToken.js
@@ -4,17 +4,13 @@ const key03 = "F3sT8JN08Nb8vZX3cQr6nk,kl9jnjj7YBH88"
 
 const getToken = async ( req , res )=>{
     try{
-        const cookie = req.cookies["4eb12nsb433nsh1ma7SHD7nsia8"]
-        if( cookie ){
+       
 
             const hash = await bcrypt.hash( "F3sT8JN08Nb8vZX3cQr6nk,kl9jnjj7YBH88" , 12 )
             const key00 = generaJWT( hash )
             res.cookie( "4eb12nsb433nsh1ma7SHD7nsia8" , key00 , { httpOnly : false } )
             res.status( 200 ).json( {  message : "success"} )
-        }
-        else{
-            res.status( 400 ).json( {  message : "CANNOT SET COOKIE" } )
-        }
+    
         
     }
     catch( error ){
