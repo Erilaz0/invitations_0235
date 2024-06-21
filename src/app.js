5353c717690eeb7777019da0cacf9be71be59a59
greenlaz <erilazerilaz@gmail.com>
2024-06-21 17:21:07 -0300

    adding-new-url

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin:"https://6668d5880da96e24091e35b7--fanciful-lebkuchen-63fcb5.netlify.app",
+  origin:[ "https://6668d5880da96e24091e35b7--fanciful-lebkuchen-63fcb5.netlify.app" , "https://6675df35543aaee7bded7432--beamish-sunburst-b9ba07.netlify.app" ],
   credentials: true
 }));
 app.engine("handlebars", handleBars.engine())
